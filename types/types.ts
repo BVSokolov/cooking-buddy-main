2503e7af0e9649849919973755d0bb8e014dcf2f
BVSokolov <sokolov.branimir@gmail.com>
2025-07-08 18:36:11 +0200

    updated plans for db work (i should probably find a better file to do this in)

--- types/types.ts
+++ types/types.ts
@@ -25,15 +25,20 @@ export type NewRecipeFormData = {
   }>
 }
 
-// FIRST THING FIRST UNFUCK FUCKING GIT SO THAT I CAN COMMIT IN FUCKING PIECE
-// FFFFFFFFFFFFFFFFUUUUUUUUUUUUUUUUUUUUUUUCCCCKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK
+// i have to make four tables next - ingredient, recipeIngredient, recipeSection and recipeStep
 
-// i have to make two tables next - recipeIngredient and recipeStep
-// recipeIngredient columns will be id, recipeId, name, amount
-// recipeStep columns will be id, recipeId, number (as in step number, first, second etc), text
-// when creating a new recipe we first create it
-// then using its id we create an entry for each ingredient in the recipeIngredient table
-// then using its id we create an entry for each step in the recipeStep table
+// ingredient columns will be id, name
+// recipeSection coulmns will be id, recipeId, name
+// recipeIngredient columns will be id, ingredientId, recipeSectionId, recipeId, amount: int, unit of measurement: enum - amount...
+// ... will be a dropdown on UI side so it's always a positive number, UOM will be enum
+// recipeStep columns will be id, recipeId, recipeSectionId, number (as in step number, first, second etc), text
+
+// when creating a new recipe we first create an entry in recipe table
+// then we create each ingredient (if it doesn't exist) in the ingredient table and return its id
+// then (if there are any ingredient sections in the recipe) we create an entry for each section in the recipeSection table and return its id
+// then using recipeId and ingredientId (and recipeSectionId if available)...
+// ...we create an entry for each ingredient in the recipeIngredient table along amount and UOM
+// then using recipeId we create an entry for each step in the recipeStep table
 export type NewRecipeData = {
   name: string
   timeInAdvance: string
