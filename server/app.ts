b8e634e7b227eccbe9165f7cb01f129b80b66a46
BVSokolov <sokolov.branimir@gmail.com>
2025-07-14 19:53:03 +0200

    middleware changes + added db and transaction objects to ctx + added global error handler which also rolls back transaction

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ const Koa = require('koa')
 const BodyParser = require('koa-bodyparser')
 const Logger = require('koa-logger')
 const cors = require('koa-cors')
+import {db} from './src/db/db'
 import {router} from './src/routes'
 
 const app = new Koa()
@@ -10,9 +11,27 @@ app
   .use(Logger())
   .use(cors())
   .use(BodyParser())
-  .use((ctx, next) => {
+  .use(async (ctx, next) => {
+    const trx = await db.transaction()
+    ctx.db = db
+    ctx.trx = trx
     ctx.body = ctx.request.body
     return next()
+  })
+  .use(async (ctx, next) => {
+    try {
+      await next()
+    } catch (err) {
+      ctx.trx.rollback()
+      console.log('ROLLED BACK')
+
+      ctx.status = err.status || 500
+      ctx.body = {
+        success: false,
+        message: err.message || 'Internal Server Error',
+      }
+      ctx.app.emit('error', err, ctx)
+    }
   })
   .use(router.routes())
   .use(router.allowedMethods())
